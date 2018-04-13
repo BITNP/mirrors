e2b3bd1a634f902c875df50f1c98c4801e60d270
zw459123678 <zeng32@qq.com>
2018-04-14 00:59:12 +0800

    代码
    精简代码格式

--- node_server/router.js
+++ node_server/router.js
@@ -3,8 +3,7 @@ var ifDebug = true;
 
 var fs = require('fs');
 var render = require('./render');
-// var rh = require("./requestHandlers");
-// 路由文件缓存，缓冲经常修改的动态资源，如，图片
+// 缓存区
 var buffer = {};
 
 // 设置文件缓存过期时间
@@ -14,16 +13,7 @@ function clearBuffer(){
 	buffer = {};
 }
 
-
-String.prototype.in_array = function(arr) {
-	for(item in arr) {
-		if(this == arr[item])
-			return true;
-	}
-	return false;
-};
-
-
+String.prototype.in_array = function(arr) { for(item in arr) if(this == arr[item]) return true; return false; };
 
 function route(pathname) {
 	if (ifDebug) console.log("About to route a request for " + pathname);
@@ -32,34 +22,23 @@ function route(pathname) {
 		// 访问缓存
 		if(buffer[pathname]) return buffer[pathname];
 
-		if(pathname.in_array(['/', '/index', '/index/']))
-			return (buffer[pathname] = render.render(fs.readFileSync('./index.html')));
-
-		if(pathname.match(/^\/download/) != null)
-			return (buffer[pathname] = render.render(fs.readFileSync("./download.html")));
-
-		if(pathname.match(/^\/help/) != null)
-			return (buffer[pathname] = render.render(fs.readFileSync("./help.html")));
-
-		if(pathname.match(/^\/mirror/) != null)
-			return (buffer[pathname] = render.render(fs.readFileSync("./mirror.html")));
+		if(pathname.in_array(['/', '/index', '/index/'])) 	return (buffer[pathname] = render.render(fs.readFileSync("./index.html")));
+		if(pathname.match(/^\/download/) != null) 			return (buffer[pathname] = render.render(fs.readFileSync("./download.html")));
+		if(pathname.match(/^\/help/) != null) 				return (buffer[pathname] = render.render(fs.readFileSync("./help.html")));
+		if(pathname.match(/^\/mirror/) != null) 			return (buffer[pathname] = render.render(fs.readFileSync("./mirror.html")));
 
 		buffer[pathname] = fs.readFileSync('.'+pathname);
 		return buffer[pathname];
 	}
 	catch(err) {
 		// 加载默认图片
-		if (pathname.match(/(\.jpg)|(\.png)/) != null)
-			return buffer.defaultImg || (buffer.defaultImg = fs.readFileSync('./Assets/img/default.png'));
+		if (pathname.match(/(\.jpg)|(\.png)/) != null) return buffer.defaultImg || (buffer.defaultImg = fs.readFileSync('./Assets/img/default.png'));
 		// 加载默认帮助文件
-		if (pathname.match(/\.md/) != null)
-			return buffer.defaultMD || (buffer.defaultMD = fs.readFileSync('./_help/error.md'));
+		if (pathname.match(/\.md/) != null) return buffer.defaultMD || (buffer.defaultMD = fs.readFileSync('./_help/error.md'));
 
 		if (ifDebug) console.log("No request handler found for " + pathname);
 		return buffer.notFound || (buffer.notFound = render.render(fs.readFileSync('./404.html')));
 	}
 }
-
-
 
 exports.route = route;
